38cf07e806106eab5b382dcbe8f83b0e2d694a0b
Ricardo Tovar <tricardo003@gmail.com>
2023-06-05 23:44:54 -0500

    add karma.conf.js file

---
+++ karma.conf.js
@@ -0,0 +1,29 @@
+module.exports = function (config) {
+    config.set({
+        basePath: '../',
+
+        files: [
+            'app/lib/angular/angular.js',
+            'app/lib/angular/angular-*.js',
+            'test/lib/angular/angular-mocks.js',
+            'app/js/**/*.js',
+            'test/unit/**/*.js'
+        ],
+
+        frameworks: ['jasmine'],
+
+        autoWatch: true,
+
+        browsers: ['Chrome'],
+        customLaunchers: {
+            ChromeHeadlessCI: {
+              base: 'ChromeHeadless',
+              flags: ['--no-sandbox']
+            }
+          },
+        junitReporter: {
+            outputFile: 'test_out/unit.xml',
+            suite: 'unit'
+        }
+    });
+};
