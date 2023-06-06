f14960e9d01778e40a6b56a3837d23732530997a
Ricardo Tovar <tricardo003@gmail.com>
2023-06-05 23:50:57 -0500

    add karma.conf.js file v3

--- karma.conf.js
+++ karma.conf.js
@@ -2,10 +2,16 @@ process.env.CHROME_BIN = require('puppeteer').executablePath() // IMPORTANT!
 
 module.exports = function(config) {
   config.set({
-    browsers: ['ChromeHeadless', 'Firefox'], 
+    browsers: ['ChromeHeadless'], 
     plugins: [
         require('karma-jasmine'),
         require('karma-chrome-launcher'), 
-    ]
+    ],
+    customLaunchers: {
+        ChromeHeadlessCI: {
+          base: 'ChromeHeadless',
+          flags: ['--no-sandbox']
+        }
+      },
   })
 }
